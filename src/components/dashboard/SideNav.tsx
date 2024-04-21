c2d4ae4594a680cff3e6857c42a132a9960a76fd
Ganesh kale <ganeshdoitnowh100@gmail.com>
2024-04-21 23:40:04 +0530

    feat dashboard side nav

---
+++ src/components/dashboard/SideNav.tsx
@@ -0,0 +1,11 @@
+import BrandLogo from "../Brand-logo";
+function SideNav() {
+  return (
+    <div>
+      <div className="w-full h-10 dark:bg-black dark:text-white  text-black bg-white">
+        <BrandLogo />
+      </div>
+    </div>
+  );
+}
+export default SideNav;
