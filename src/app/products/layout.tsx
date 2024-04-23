1fe1e2786d942a27c512cf5183e9d73e9a981889
Ganesh kale <ganeshdoitnowh100@gmail.com>
2024-04-23 09:52:26 +0530

    style changes in layout

--- src/app/products/layout.tsx
+++ src/app/products/layout.tsx
@@ -5,9 +5,9 @@ function layout({ children }: { children: React.ReactNode }) {
     <div className="min-h-screen w-full bg-white">
       <ProductProvider>
         <CartProvider>
-          <div className="">
-            <div></div>
-            <div>{children}</div>
+          <div className="flex h-screen md:max-w-4xl overflow-hidden flex-col md:flex-row">
+            <div className="flex-none hidden md:block"></div>
+            <div className=" flex-grow">{children}</div>
           </div>
         </CartProvider>
       </ProductProvider>
