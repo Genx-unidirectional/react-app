5d51864eca502443ca01cd6172ee401b8c433bf8
Ganesh kale <ganeshdoitnowh100@gmail.com>
2024-04-23 20:40:38 +0530

    style css fixed

--- src/components/products/ChangingPanel.tsx
+++ src/components/products/ChangingPanel.tsx
@@ -1,5 +1,33 @@
+import useCart from "@/hooks/useCart";
+
 type Props = {};
 function ChangingPanel() {
-  return <div className="md:w-5/6 w-full"></div>;
+  const { totalPrice, totalProducts, dispatch, Reducer_Actions } = useCart();
+  return (
+    <div className="  flex border md:hidden border-black w-full bg-black md: text-white p-2 rounded-lg fixed z-10  bottom-4  justify-between  items-center ">
+      <div className="flex items-center gap-4">
+        <p className="font-medium">
+          {" "}
+          Total price : <span className="text-indigo-300">
+            {" "}
+            {totalPrice}
+          </span>{" "}
+        </p>
+        <p className="font-medium">
+          {" "}
+          Total product :{" "}
+          <span className="text-indigo-300"> {totalProducts}</span>{" "}
+        </p>
+      </div>
+      <button
+        onClick={() => {
+          dispatch({ type: Reducer_Actions.SUBMIT });
+        }}
+        className="p-1 bg-white text-black  rounded-lg font-bold"
+      >
+        Buy
+      </button>
+    </div>
+  );
 }
 export default ChangingPanel;
