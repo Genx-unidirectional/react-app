3ccc1ee3e058edc1a84e4f3b9da69fba3b94e704
Ganesh kale <ganeshdoitnowh100@gmail.com>
2024-04-23 20:38:54 +0530

    style css layout fixing

--- src/app/products/layout.tsx
+++ src/app/products/layout.tsx
@@ -1,17 +1,31 @@
-import { ProductProvider, CartProvider } from "@/context/products";
+"use client";
+import ProductsNav from "@/components/products/Products-Nav";
+import { ProductProvider } from "@/context/products/productProvider";
+import { CartProvider } from "@/context/products/cartProvider";
+import { useState } from "react";
+import ChangingPanel from "@/components/products/ChangingPanel";
+import SideBar from "@/components/products/SideBar";
 
 function layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="min-h-screen w-full bg-white">
-      <ProductProvider>
-        <CartProvider>
-          <div className="flex h-screen md:max-w-4xl overflow-hidden flex-col md:flex-row">
-            <div className="flex-none hidden md:block"></div>
-            <div className=" flex-grow">{children}</div>
+    <ProductProvider>
+      <CartProvider>
+        <div className="min-h-screen md:h-screen bg-white md:overflow-hidden ">
+          <div className=" fixed z-10   bg-white top-0 mx-auto w-full ">
+            <ProductsNav />
           </div>
-        </CartProvider>
-      </ProductProvider>
-    </div>
+          <div className="flex h-screen mt-20  gap-2  mx-auto md:max-w-4xl  flex-col md:flex-row mb-12">
+            <div className="flex-none  hidden overflow-hidden md:w-60 md:block border-r border-slate-500">
+              <SideBar />
+            </div>
+            <div className=" flex-grow  h-full md:overflow-y-auto">
+              {children}
+            </div>
+            <ChangingPanel />
+          </div>
+        </div>
+      </CartProvider>
+    </ProductProvider>
   );
 }
 export default layout;
--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,4 +1,27 @@
+"use client";
+import useProducts from "@/hooks/useProduct";
+import useCart from "@/hooks/useCart";
+import Product from "@/components/products/Product";
 function ProductPage() {
-  return <div>ProductPage</div>;
+  const { products } = useProducts();
+  const { dispatch, cart, Reducer_Actions } = useCart();
+  let pageContent = products.map((product) => {
+    const inCart = cart.some((cartItem) => cartItem.sku === product.sku);
+    return (
+      <Product
+        key={product.sku}
+        dispatch={dispatch}
+        Reducer_Actions={Reducer_Actions}
+        inCart={inCart}
+        product={product}
+      />
+    );
+  });
+
+  return (
+    <main className="grid grid-cols-1   sm:grid-cols-2  mb-20 md:grid-cols-2 lg:grid-cols-3 gap-2">
+      {pageContent}
+    </main>
+  );
 }
 export default ProductPage;
