1a8709c37ba5acd562973cad192ac5251f070772
Ganesh kale <ganeshdoitnowh100@gmail.com>
2024-04-23 09:34:27 +0530

    feat useCart hook to provide cart context

---
+++ src/hooks/useCart.tsx
@@ -0,0 +1,8 @@
+import { CartContext, CartContextState } from "@/context/products";
+import { useContext } from "react";
+
+const useCart = (): CartContextState => {
+  return useContext(CartContext);
+};
+
+export default useCart;
