7e56232be1e4f87990181042f5bc19ee473f65d7
Ganesh kale <ganeshdoitnowh100@gmail.com>
2024-04-23 16:49:25 +0530

    style css changes

--- src/components/products/Product.tsx
+++ src/components/products/Product.tsx
@@ -13,9 +13,15 @@ type Props = {
 
 function Product({ inCart, dispatch, Reducer_Actions, product }: Props) {
   return (
-    <div className="flex flex-col">
-      <img src={`/images/${product.sku}.jpg`} alt={product.name} />
-      <h3>{product.name}</h3>
+    <div className="flex flex-col hover:scale-[1.012] rounded-lg mb-4 shadow-sm shadow-black p-2 w-5/6 md:w-full gap-2  mx-auto   ">
+      <div className=" w-full overflow-hidden">
+        <img
+          className="mx-auto  rounded-lg"
+          src={`/images/${product.sku}.jpg`}
+          alt={product.name}
+        />
+      </div>
+      <h3 className="text-2xl md:text-3xl font-medium">{product.name}</h3>
       {inCart ? <p>In Cart</p> : null}
       <p>
         {new Intl.NumberFormat("en-US", {
