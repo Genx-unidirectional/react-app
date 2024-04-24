636b294c46d5098fdc0ac8744beee05ced420ef2
Ganesh kale <ganeshdoitnowh100@gmail.com>
2024-04-25 00:41:24 +0530

    feat link populate with mini config

--- src/components/mini/MiniSidebar.tsx
+++ src/components/mini/MiniSidebar.tsx
@@ -1,4 +1,27 @@
+import Link from "next/link";
+import { ChevronRight } from "lucide-react";
+import { MINI_PROJECTS_ARR } from "@/config/mini";
 function MiniSidebar() {
-  return <div>MiniSidebar</div>;
+  return (
+    <div className=" p-2 text-white">
+      <h1 className="text-2xl lg:text-4xl font-medium mb-4 tracking-wider">
+        Mini Projects
+      </h1>
+      <div>
+        {MINI_PROJECTS_ARR.map((name, idx) => {
+          return (
+            <Link
+              key={idx}
+              className="text-blue-500 flex lg:text-xl"
+              href={`#${idx.toString()}`}
+            >
+              {name}
+              <ChevronRight />
+            </Link>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 export default MiniSidebar;
