99c5952f11ef7cf2bf77f7478f1bbd82f8618cd7
Ganesh kale <ganeshdoitnowh100@gmail.com>
2024-04-24 23:59:23 +0530

    bug naming changes in sidebar

--- src/components/products/SideBar.tsx
+++ src/components/products/SideBar.tsx
@@ -5,7 +5,7 @@ function SideBar() {
   return (
     <div className="h-full flex flex-col p-3 items-start justify-start">
       <p>Total Price : {totalPrice}</p>
-      <p>Total Price : {totalProducts}</p>
+      <p>Total Products : {totalProducts}</p>
       <button
         onClick={() => {
           dispatch({ type: Reducer_Actions.SUBMIT });
