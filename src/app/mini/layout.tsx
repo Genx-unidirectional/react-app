203e4ee42adcea079d0ce9d14bc4bdbf67ceb22e
Ganesh kale <ganeshdoitnowh100@gmail.com>
2024-04-24 23:55:46 +0530

    feat mini project route with page and layout

---
+++ src/app/mini/layout.tsx
@@ -0,0 +1,22 @@
+import MiniNavbar from "@/components/mini/MiniNavbar";
+import MiniSidebar from "@/components/mini/MiniSidebar";
+
+function layout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="min-h-screen  w-full">
+      <header className="fixed top-0 w-full z-10 bg-black text-white">
+        <MiniNavbar />
+      </header>
+      <div className="flex">
+        <div className="md:block hidden border-l border-slate-500 bg-black flex-none w-56 lg:w-72">
+          <MiniSidebar />
+        </div>
+
+        <div className="min-h-screen  flex-grow md:h-screen bg-slate-900 ">
+          {children}
+        </div>
+      </div>
+    </div>
+  );
+}
+export default layout;
---
+++ src/app/mini/page.tsx
@@ -0,0 +1,4 @@
+function page() {
+  return <div className="">page</div>;
+}
+export default page;
