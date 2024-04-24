0ab99a8f34bd581663954c2219e6defca7e4d1b2
Ganesh kale <ganeshdoitnowh100@gmail.com>
2024-04-25 00:38:08 +0530

    style items centered in logo

--- src/components/Brand-logo.tsx
+++ src/components/Brand-logo.tsx
@@ -1,14 +1,17 @@
 import { cn } from "@/lib/utils";
 import { lusitana } from "./font";
 import { AtomIcon } from "lucide-react";
+import Link from "next/link";
 type Props = {
   className?: string;
 };
 function BrandLogo({ className }: Props) {
   return (
     <div className={cn(lusitana.className, "flex items-center", className)}>
-      <AtomIcon className="w-10 h-10" />
-      <p className="text-3xl font-bold">Gex</p>
+      <Link href={"/"} className="flex items-center">
+        <AtomIcon className="w-10 h-10" />
+        <p className="text-3xl font-bold ">Gex</p>
+      </Link>
     </div>
   );
 }
