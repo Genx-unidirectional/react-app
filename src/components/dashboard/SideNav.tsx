3ac639abe6945c6868d75fc78c8a7458ce32cc44
Ganesh kale <ganeshdoitnowh100@gmail.com>
2024-04-25 19:51:26 +0530

    feat nav and sidebar for dashboard

--- src/components/dashboard/SideNav.tsx
+++ src/components/dashboard/SideNav.tsx
@@ -2,8 +2,8 @@ import BrandLogo from "../Brand-logo";
 import { ModeToggle } from "../ModeToggle";
 function SideNav() {
   return (
-    <div className="">
-      <div className="w-full p-2 h-10 bg- flex justify-between bg-background text-foreground ">
+    <div className="py-2 px-2 border-b border-slate-400">
+      <div className="w-full  items-center h-10 bg- flex justify-between bg-background text-foreground ">
         <BrandLogo />
         <ModeToggle />
       </div>
