2d1e0f2e04eb852b086495686b1ed836c4df1d06
Ganesh kale <ganeshdoitnowh100@gmail.com>
2024-04-25 18:44:08 +0530

    feat  font for main page

--- src/components/font.ts
+++ src/components/font.ts
@@ -1,5 +1,5 @@
-import { Lusitana, Inter } from "next/font/google";
+import { Lusitana, Inter, Poppins } from "next/font/google";
 
 export const lusitana = Lusitana({ subsets: ["latin"], weight: "700" });
-
+export const poppins = Poppins({ subsets: ["latin"], weight: "600" });
 export const inter = Inter({ subsets: ["latin"] });
