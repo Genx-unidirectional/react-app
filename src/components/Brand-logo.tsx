4dbe278feeab43098fcebf64cd1516cff997bf15
Ganesh kale <ganeshdoitnowh100@gmail.com>
2024-04-25 18:44:52 +0530

    style css fix in logo

--- src/components/Brand-logo.tsx
+++ src/components/Brand-logo.tsx
@@ -1,3 +1,5 @@
+"use client";
+import { usePathname } from "next/navigation";
 import { cn } from "@/lib/utils";
 import { lusitana } from "./font";
 import { AtomIcon } from "lucide-react";
@@ -6,8 +8,9 @@ type Props = {
   className?: string;
 };
 function BrandLogo({ className }: Props) {
+  const pathName = usePathname();
   return (
-    <div className={cn(lusitana.className, "flex items-center", className)}>
+    <div className={cn(lusitana.className, "flex items-center ", className)}>
       <Link href={"/"} className="flex items-center">
         <AtomIcon className="w-10 h-10" />
         <p className="text-3xl font-bold ">Gex</p>
