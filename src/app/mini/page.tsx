bf652c128dd01ea4fdca202f3ada16b1f32940b3
Ganesh kale <ganeshdoitnowh100@gmail.com>
2024-04-26 13:46:24 +0530

    feat component added

--- src/app/mini/page.tsx
+++ src/app/mini/page.tsx
@@ -1,12 +1,25 @@
+import ControlledForm from "@/components/mini/forms/ControlledForm";
+import ReactHookForm from "@/components/mini/forms/ReactHookForm";
+import SimpleForm from "@/components/mini/forms/SimpleForm";
 import { ReactElement } from "react";
 
 function page() {
-  const componentArr: ReactElement[] = [];
+  const componentArr: ReactElement[] = [
+    <SimpleForm />,
+    <ControlledForm />,
+    <ReactHookForm />,
+  ];
   return (
     <div className="mt-[56.8px] text-white">
       {componentArr.map((component, idx) => {
         return (
-          <div key={idx} id={idx.toString()} className="h-full w-full"></div>
+          <div
+            key={idx}
+            id={idx.toString()}
+            className="h-screen flex justify-center items-center w-full"
+          >
+            {component}
+          </div>
         );
       })}
     </div>
