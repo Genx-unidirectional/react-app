b08ed9db23db56f44c48874b32f360952ad23fde
Ganesh kale <ganeshdoitnowh100@gmail.com>
2024-04-28 16:50:23 +0530

    feat server side validation added

--- src/components/mini/forms/ReactHookForm.tsx
+++ src/components/mini/forms/ReactHookForm.tsx
@@ -1,8 +1,9 @@
 "use client";
 import { useForm } from "react-hook-form";
-import { FormData, UserSchema } from "@/types/form";
+import { FormData, UserSchema, ValidFieldNames } from "@/types/form";
 import FormField from "./FormField";
 import { zodResolver } from "@hookform/resolvers/zod";
+import axios from "axios";
 
 function Form() {
   const {
@@ -13,7 +14,35 @@ function Form() {
   } = useForm<FormData>({ resolver: zodResolver(UserSchema) });
 
   const onSubmit = async (data: FormData) => {
-    console.log("SUCCESS", data);
+    try {
+      const response = await axios.post("/api/form", data); // Make a POST request
+      const { errors = {} } = response.data; // Destructure the 'errors' property from the response data
+      console.log(response);
+      // Define a mapping between server-side field names and their corresponding client-side names
+      const fieldErrorMapping: Record<string, ValidFieldNames> = {
+        email: "email",
+        githubUrl: "githubUrl",
+        yearsOfExperience: "yearsOfExperience",
+        password: "password",
+        confirmPassword: "confirmPassword",
+      };
+
+      // Find the first field with an error in the response data
+      const fieldWithError = Object.keys(fieldErrorMapping).find(
+        (field) => errors[field]
+      );
+
+      // If a field with an error is found, update the form error state using setError
+      if (fieldWithError) {
+        // Use the ValidFieldNames type to ensure the correct field names
+        setError(fieldErrorMapping[fieldWithError], {
+          type: "server",
+          message: errors[fieldWithError],
+        });
+      }
+    } catch (error) {
+      alert("Submitting form failed!");
+    }
   };
 
   return (
