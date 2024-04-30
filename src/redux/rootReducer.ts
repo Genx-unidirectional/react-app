30583cca23c82fdc833985b653f59b20bb5b03fd
Ganesh kale <ganeshdoitnowh100@gmail.com>
2024-05-01 00:17:23 +0530

    feat userSlice added to rootReducer

--- src/redux/rootReducer.ts
+++ src/redux/rootReducer.ts
@@ -2,9 +2,10 @@ import { combineReducers } from "redux";
 
 // import slices
 import mainSliceReducer from "./features/task/mainSlice";
-
+import userSlice from "./features/users/userSlice";
 const rootReducer = combineReducers({
   cards: mainSliceReducer,
+  user: userSlice,
 });
 
 export default rootReducer;
