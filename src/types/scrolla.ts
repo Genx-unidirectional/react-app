76f06690d81e355552fa27aa48f4026b8154de77
Ganesh kale <ganeshdoitnowh100@gmail.com>
2024-05-02 21:56:17 +0530

    update changes in types for scrolla project

--- src/types/scrolla.ts
+++ src/types/scrolla.ts
@@ -31,3 +31,45 @@ export const ImageDataSchema: ZodType<Product[]> = z.array(
 );
 
 export type ImageDataType = z.infer<typeof ImageDataSchema>;
+
+export const CATEGORIES = [
+  "smartphones",
+  "laptops",
+  "fragrances",
+  "skincare",
+  "groceries",
+  "home-decoration",
+  "furniture",
+  "tops",
+  "womens-dresses",
+  "womens-shoes",
+  "mens-shirts",
+  "mens-shoes",
+  "mens-watches",
+  "womens-watches",
+  "womens-bags",
+  "womens-jewellery",
+  "sunglasses",
+  "automotive",
+  "motorcycle",
+  "lighting",
+] as const;
+
+export const defaultPriceRange: [number, number] = [10, 1749];
+export const defaultRating: [number, number] = [1, 5];
+
+export const discountPercentage: [number, number] = [2, 18]; //this is vague
+
+export const ProductFilterValidator = z.object({
+  categories: z.array(z.enum(CATEGORIES)),
+  price: z.tuple([z.number(), z.number()]),
+  rating: z.tuple([z.number(), z.number()]),
+});
+
+export type ProductFilterState = Omit<
+  z.infer<typeof ProductFilterValidator>,
+  "price" | "rating"
+> & {
+  price: { isCustom: boolean; range: [number, number] };
+  rating: { isCustom: boolean; range: [number, number] };
+};
