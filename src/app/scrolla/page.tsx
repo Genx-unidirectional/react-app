cd54999923972a688ae45edd6b427f2720253ebd
Ganesh kale <ganeshdoitnowh100@gmail.com>
2024-05-02 22:04:26 +0530

    bug css bugs fixed

--- src/app/scrolla/page.tsx
+++ src/app/scrolla/page.tsx
@@ -1,19 +1,17 @@
-"use client";
 import ImageCardList from "@/components/scrolla/ImageCardList";
 import ScrollHead from "@/components/scrolla/ScrollHead";
 import SideScroll from "@/components/scrolla/SideScroll";
 import { useState } from "react";
 
 function page() {
-  const [filterArr] = useState();
   return (
     <div className="min-h-screen lg:overflow-hidden  w-full">
       <ScrollHead />
-      <main className="flex h-full  w-full mt-[72.8px] flex-col lg:flex-row">
-        <div className="lg:block hidden overflow-hidden relative min-h-screen  border-slate-500 bg-black flex-none w-64 lg:w-64  border-r ">
+      <main className="flex   mt-[72.8px] flex-col lg:flex-row">
+        <div className="lg:block hidden relative min-h-screen  border-slate-500 bg-black flex-none lg:w-72  border-r ">
           <SideScroll />
         </div>
-        <div className=" lg:flex-grow h-full bg-white  w-full  ">
+        <div className=" lg:flex-grow lg:min-h-screen bg-white   w-full  ">
           <ImageCardList />
         </div>
       </main>
