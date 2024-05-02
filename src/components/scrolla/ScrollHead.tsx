de0dae00d108710c7208a4b9a30ec1e15bf1e36b
Ganesh kale <ganeshdoitnowh100@gmail.com>
2024-05-02 22:00:45 +0530

    update scrollHead with filterSection

--- src/components/scrolla/ScrollHead.tsx
+++ src/components/scrolla/ScrollHead.tsx
@@ -1,4 +1,5 @@
 import BrandLogo from "../Brand-logo";
+import FilterSection from "./FilterSection";
 function ScrollHead() {
   return (
     <header
@@ -6,6 +7,9 @@ function ScrollHead() {
       className="w-full z-[100] py-4  px-1 sm:px-4 l flex transition-all delay-500 bg-black justify-between top-0 items-center fixed  border-b border-slate-700  text-white"
     >
       <BrandLogo className="text-white" />
+      <div className="fxl:hidden">
+        <FilterSection />
+      </div>
     </header>
   );
 }
