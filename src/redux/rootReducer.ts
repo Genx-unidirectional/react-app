7f42fc05595bdd7788631f08f4ae90796db0f91b
Ganesh kale <ganeshdoitnowh100@gmail.com>
2024-05-02 21:55:38 +0530

    update root reducer with new reducers

--- src/redux/rootReducer.ts
+++ src/redux/rootReducer.ts
@@ -4,10 +4,12 @@ import { combineReducers } from "redux";
 import mainSliceReducer from "./features/task/mainSlice";
 import userSliceReducer from "./features/users/userSlice";
 import productSliceReducer from "./features/products/productSlice";
+import filterSliceReducer from "./features/filterProduct/filterSlice";
 const rootReducer = combineReducers({
   cards: mainSliceReducer,
   users: userSliceReducer,
   productsArray: productSliceReducer,
+  filters: filterSliceReducer,
 });
 
 export default rootReducer;
