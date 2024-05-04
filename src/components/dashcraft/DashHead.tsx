c32a6edb9acc3ff2b61e9fc71cd557dec07d58da
Ganesh kale <ganeshdoitnowh100@gmail.com>
2024-05-04 23:27:55 +0530

    feat header for dashcraft

---
+++ src/components/dashcraft/DashHead.tsx
@@ -0,0 +1,14 @@
+import BrandLogo from "../Brand-logo";
+
+function DashHead() {
+  return (
+    <header
+      id="navbar"
+      className="w-full z-[100] py-4  px-1 sm:px-4 l flex transition-all delay-500 bg-black justify-between top-0 items-center fixed  border-b border-slate-700  text-white"
+    >
+      <BrandLogo className="text-white" />
+      <div className="fxl:hidden"></div>
+    </header>
+  );
+}
+export default DashHead;
