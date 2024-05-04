759725a21c8d3ccce96339f62846074512ff029c
Ganesh kale <ganeshdoitnowh100@gmail.com>
2024-05-04 10:47:24 +0530

    feat nav-links added

--- src/components/dashcraft/dashboard/Nav-links.tsx
+++ src/components/dashcraft/dashboard/Nav-links.tsx
@@ -1,12 +1,47 @@
-import { HomeIcon } from "lucide-react";
+"use client";
 
-function NavLinks() {
-  const links = [
-    {
-      url: "",
-      linkIcon: <HomeIcon />,
-    },
-  ];
-  return <div>NavLinks</div>;
+import { UserRound, Home, File } from "lucide-react";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+import clsx from "clsx";
+// Map of links to display in the side navigation.
+// Depending on the size of the application, this would be stored in a database.
+const links = [
+  { name: "Home", href: "/dashcraft/dashboard", icon: Home },
+  {
+    name: "Invoices",
+    href: "/dashcraft/dashboard/invoices",
+    icon: File,
+  },
+  {
+    name: "Customers",
+    href: "/dashcraft/dashboard/customers",
+    icon: UserRound,
+  },
+];
+
+export default function NavLinks() {
+  const pathName = usePathname();
+  return (
+    <>
+      {links.map((link) => {
+        const LinkIcon = link.icon;
+        return (
+          <Link
+            key={link.name}
+            href={link.href}
+            className={clsx(
+              "flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-slate-500 hover:text-slate-50 md:flex-none md:justify-start md:p-2 md:px-3",
+              {
+                "bg-slate-500 text-slate-50": link.href === pathName,
+              }
+            )}
+          >
+            <LinkIcon className="w-6" />
+            <p className="hidden md:block">{link.name}</p>
+          </Link>
+        );
+      })}
+    </>
+  );
 }
-export default NavLinks;
