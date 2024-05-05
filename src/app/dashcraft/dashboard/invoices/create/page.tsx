19abce73e772293564b9ec70b3c8c160876f7f57
Ganesh kale <ganeshdoitnowh100@gmail.com>
2024-05-05 09:02:27 +0530

    update commented data fetching function

--- src/app/dashcraft/dashboard/invoices/create/page.tsx
+++ src/app/dashcraft/dashboard/invoices/create/page.tsx
@@ -3,7 +3,7 @@ import Breadcrumbs from "@/components/dashcraft/invoices/breadcrumbs";
 import { fetchCustomers } from "@/lib/data";
 
 export default async function Page() {
-  const customers = await fetchCustomers();
+  // const customers = await fetchCustomers();
 
   return (
     <main>
