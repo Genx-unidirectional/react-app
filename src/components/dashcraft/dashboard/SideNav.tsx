bd552f9622cb36629e3196c427531507f2ecea4e
Ganesh kale <ganeshdoitnowh100@gmail.com>
2024-05-05 22:50:17 +0530

    update: form with logout functionality

--- src/components/dashcraft/dashboard/SideNav.tsx
+++ src/components/dashcraft/dashboard/SideNav.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import NavLinks from "@/components/dashcraft/dashboard/Nav-links";
 import BrandLogo from "@/components/Brand-logo";
 import { PowerIcon } from "lucide-react";
+import { signOut } from "@/auth";
 export default function SideNav() {
   return (
     <div className="flex h-full flex-col px-3 py-4 md:px-2">
@@ -11,7 +12,12 @@ export default function SideNav() {
       <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
         <NavLinks />
         <div className="hidden h-auto w-full grow rounded-md bg-gray-50 md:block"></div>
-        <form>
+        <form
+          action={async () => {
+            "use server";
+            await signOut();
+          }}
+        >
           <button className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-slate-500 hover:text-slate-50 md:flex-none md:justify-start md:p-2 md:px-3">
             <PowerIcon className="w-6" />
             <div className="hidden md:block">Sign Out</div>
