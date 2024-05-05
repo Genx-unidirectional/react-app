0fe963c25c2db386274f43b3f443e4a133096904
Ganesh kale <ganeshdoitnowh100@gmail.com>
2024-05-05 22:47:57 +0530

    update: login form with authentication

--- src/components/dashcraft/login-form.tsx
+++ src/components/dashcraft/login-form.tsx
@@ -2,10 +2,13 @@ import { lusitana } from "@/components/font";
 
 import { ArrowRightIcon, AtSign, KeyRound, CircleAlert } from "lucide-react";
 import { Button } from "./button";
-
+import { useFormState, useFormStatus } from "react-dom";
+import { authenticate } from "@/lib/actions";
 export default function LoginForm() {
+  const [errorMessage, dispatch] = useFormState(authenticate, undefined);
+
   return (
-    <form className="space-y-3">
+    <form action={dispatch} className="space-y-3">
       <div className="flex-1 rounded-lg bg-white px-6 pb-4 pt-8">
         <h1 className={`${lusitana.className} mb-3 text-2xl`}>
           Please log in to continue.
@@ -51,9 +54,17 @@ export default function LoginForm() {
             </div>
           </div>
         </div>
-        <CircleAlert />
-        <div className="flex h-8 items-end space-x-1">
-          {/* Add form errors here */}
+        <div
+          className="flex h-8 items-end space-x-1"
+          aria-live="polite"
+          aria-atomic="true"
+        >
+          {errorMessage && (
+            <>
+              <CircleAlert className="h-5 w-5 text-red-500" />
+              <p className="text-sm text-red-500">{errorMessage}</p>
+            </>
+          )}
         </div>
       </div>
     </form>
@@ -61,8 +72,9 @@ export default function LoginForm() {
 }
 
 function LoginButton() {
+  const { pending } = useFormStatus();
   return (
-    <Button className="mt-4 w-full">
+    <Button className="mt-4 w-full" aria-disabled={pending}>
       Log in <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
     </Button>
   );
