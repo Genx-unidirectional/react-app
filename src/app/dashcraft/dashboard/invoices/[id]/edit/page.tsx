659f2dfc3e035f4bc8369227bd7ac1e9fe25bb8e
Ganesh kale <ganeshdoitnowh100@gmail.com>
2024-05-05 18:59:34 +0530

    update:edit form for specific user

--- src/app/dashcraft/dashboard/invoices/[id]/edit/page.tsx
+++ src/app/dashcraft/dashboard/invoices/[id]/edit/page.tsx
@@ -1,4 +1,26 @@
-function page() {
-  return <div>page</div>;
+import Form from "@/components/dashcraft/invoices/edit-form";
+import Breadcrumbs from "@/components/dashcraft/invoices/breadcrumbs";
+import { fetchCustomers, fetchInvoiceById } from "@/lib/data";
+export default async function Page({ params }: { params: { id: string } }) {
+  const id = params.id;
+  const [invoice, customers] = await Promise.all([
+    fetchInvoiceById(id),
+    fetchCustomers(),
+  ]);
+
+  return (
+    <main>
+      <Breadcrumbs
+        breadcrumbs={[
+          { label: "Invoices", href: "/dashcraft/dashboard/invoices" },
+          {
+            label: "Edit Invoice",
+            href: `/dashcraft/dashboard/invoices/${id}/edit`,
+            active: true,
+          },
+        ]}
+      />
+      <Form invoice={invoice} customers={customers} />
+    </main>
+  );
 }
-export default page;
