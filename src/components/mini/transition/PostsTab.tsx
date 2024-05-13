633cbee18990069fdc524a1ecc93503b1a4924f6
Ganesh kale <ganeshdoitnowh100@gmail.com>
2024-05-13 20:11:47 +0530

    update: using memo to memoised the posts component

--- src/components/mini/transition/PostsTab.tsx
+++ src/components/mini/transition/PostsTab.tsx
@@ -1,14 +1,16 @@
-function PostsTab() {
+import { memo } from "react";
+
+const PostsTab = memo(function PostsTab() {
   let items: JSX.Element[] = [];
   for (let i = 0; i < 500; i++) {
     items.push(<SlowComponent index={i} />);
   }
   return <ul className=" ">{items}</ul>;
-}
+});
 export default PostsTab;
 
 function SlowComponent({ index }: { index: number }) {
   let startTime = performance.now();
-  while (performance.now() - startTime < 2) {}
+  while (performance.now() - startTime < 1) {}
   return <li>Post #{index}</li>;
 }
