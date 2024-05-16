7a7694bf3b2865b85e8c483070b9807a013eae12
Ganesh kale <ganeshdoitnowh100@gmail.com>
2024-05-16 19:30:43 +0530

    update: css fixed

--- src/app/mini/layout.tsx
+++ src/app/mini/layout.tsx
@@ -3,16 +3,16 @@ import MiniSidebar from "@/components/mini/MiniSidebar";
 
 function layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="min-h-screen md:h-screen w-full">
+    <div className="min-h-screen scrollbar-none md:h-screen w-full">
       <header className="fixed top-0 w-full z-50 bg-black text-white">
         <MiniNavbar />
       </header>
-      <div className="flex">
-        <div className="md:block hidden overflow-hidden relative min-h-screen  border-slate-500 bg-black flex-none w-56 lg:w-72 mt-[72.8px] border-r ">
+      <div className="flex relative scrollbar-none isolate w-full min-h-screen bg-slate-900">
+        <div className="z-10 mt-[72.8px] h-screen isolate hidden md:block bg-black">
           <MiniSidebar />
         </div>
 
-        <div className="min-h-screen  flex-grow   bg-slate-900 ">
+        <div className="min-h-screen md:h-screen overflow-hidden overflow-y-scroll isolate   flex-grow  z-50  scrollbar-none">
           {children}
         </div>
       </div>
